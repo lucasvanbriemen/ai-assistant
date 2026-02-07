ffa07d6061c346649f5f77b48b027ce83248e81f
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-07 13:41:07 +0100

    Remove unnecessary comment.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// SPA catch-all route (must be last)
 Route::get('/{any}', function () {
     return view('index');
 })->where('any', '.*')->name('spa.catchall');
