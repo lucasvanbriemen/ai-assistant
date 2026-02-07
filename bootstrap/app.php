66806fbab5a66626be1491d65942f7a934ad3eb1
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-07 13:58:08 +0100

    Remove unnecessary comments.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: "/up",
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
         $middleware->alias([
             "IsLoggedIn" => IsLoggedIn::class,
         ]);
