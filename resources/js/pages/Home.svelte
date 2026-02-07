0a68e236fa06e4a1a4960fe904d8fa42811246a7
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-07 14:00:08 +0100

    Remove extra styling.

--- resources/js/pages/Home.svelte
+++ resources/js/pages/Home.svelte
@@ -8,19 +8,3 @@
   </div>
 </div>
 
-<style>
-  .chat-page {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    padding: 20px;
-    background: #f5f5f5;
-  }
-
-  .chat-container {
-    flex: 1;
-    max-width: 900px;
-    margin: 0 auto;
-    width: 100%;
-  }
-</style>
