dbf27396d60b04ff46412958854b50fd9471915b
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-08 19:39:49 +0100

    Fix background color.

--- resources/js/App.svelte
+++ resources/js/App.svelte
@@ -1,7 +1,6 @@
 <script>
     import { router, initRouter } from './stores/router.svelte.js';
     import api from './lib/api.js';
-    import '@styles/App.scss';
 
     initRouter();
     window.api = api;
