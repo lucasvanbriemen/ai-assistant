080c31df6154e5989e20c9dad0063c1a30933e3f
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-14 20:13:14 +0100

    Cleanup.

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ChatController;
 use App\Http\Controllers\WebhookController;
 use App\Http\Middleware\IsLoggedIn;
 
-// Chat endpoint
 Route::post('/chat/send', [ChatController::class, 'sendMessage'])->middleware(IsLoggedIn::class);
 
 Route::prefix('webhooks')->group(function () {
