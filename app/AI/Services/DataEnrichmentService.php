8440171381ecd387ef1f43c5865a5d345bf15738
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-14 22:43:42 +0100

    Remove unnecessary function.

--- app/AI/Services/DataEnrichmentService.php
+++ app/AI/Services/DataEnrichmentService.php
@@ -24,26 +24,6 @@ class DataEnrichmentService
         return $enriched;
     }
 
-    public static function enrichCalendarEvent(array $eventData): array
-    {
-        $enriched = $eventData;
-        $enriched['attendee_entities'] = [];
-
-        foreach ($eventData['attendees'] ?? [] as $attendee) {
-            $email = self::extractEmail($attendee);
-            $name = self::extractName($attendee, $email);
-
-            if ($name) {
-                $enriched['attendee_entities'][] = [
-                    'name' => $name,
-                    'email' => $email,
-                ];
-            }
-        }
-
-        return $enriched;
-    }
-
     private static function extractEmail(string $text): ?string
     {
         if (preg_match('/<(.+?)>/', $text, $matches)) {
