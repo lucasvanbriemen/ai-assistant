f0530c58f61fbcd4e80929f922c9fa9ffd6c7624
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-14 19:55:41 +0100

    Clean up console.

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-// Scheduled tasks
 Schedule::command('cleanup:webhooks')->daily();
-
-// Inspiring quote command
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
