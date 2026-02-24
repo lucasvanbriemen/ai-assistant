ff7b183122ae7fc2d2c8c3faea08746843995be7
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-24 17:04:25 +0100

    cleanup

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,3 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
-
-Route::get('/test', function () {
-    return Inertia::render('Welcome2');
-})->name('test');
