40d58bab419fcde90e9ef790d7a93c53e166303c
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-24 17:27:44 +0100

    Use generator.

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function (Request $request) {
-    return response()->stream(function (): void {
+    return response()->stream(function (): Generator {
         foreach (['developer', 'admin'] as $string) {
-            echo $string . "\n";
-            ob_flush();
-            flush();
             sleep(2); // Simulate delay between chunks...
+            yield $string . "\n";
         }
     }, 200, ['X-Accel-Buffering' => 'no']);
 });
