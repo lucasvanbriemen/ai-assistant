e908fcfb5cb87ab67dba8af24e9b493e50abb3d3
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-24 18:19:21 +0100

    Move logic to controller.

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AiController;
-
-Route::get('/tes2t', function (Request $request) {
-    return response()->stream(function (): Generator {
-        foreach (['developer', 'admin'] as $string) {
-            sleep(2); // Simulate delay between chunks...
-            yield $string . "\n";
-        }
-    }, 200, ['X-Accel-Buffering' => 'no']);
-});
 
 Route::get('/test', [AiController::class, 'index']);
