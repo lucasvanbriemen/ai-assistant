0ab15a05d5a072f09a97706c6a09c273b26ed35b
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-24 17:16:36 +0100

    use streaming to get data

--- resources/js/pages/Welcome.svelte
+++ resources/js/pages/Welcome.svelte
@@ -1,9 +1,37 @@
 <script>
     import AppHead from '@/components/AppHead.svelte';
+
+    let result = $state('');
+
+    function click() {
+        fetch('/api/test')
+            .then((response) => {
+                const reader = response.body.getReader();
+                const decoder = new TextDecoder();
+
+                function read() {
+                    reader.read().then(({ done, value }) => {
+                        if (done) {
+                            console.log('Stream complete:', result);
+                            return;
+                        }
+                        result += decoder.decode(value, { stream: true });
+                        console.log('Received chunk:', result);
+                        read();
+                    });
+                }
+
+                read();
+            })
+            .catch((error) => {
+                console.error('Error fetching stream:', error);
+            });
+    }
 </script>
 
 <AppHead title="Home">
 </AppHead>
 
-Hello
-
+<button onclick={click}>click me</button>
+ <br>
+output: {result}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function (Request $request) {
+    return response()->stream(function (): void {
+        foreach (['developer', 'admin'] as $string) {
+            echo $string . "\n";
+            ob_flush();
+            flush();
+            sleep(2); // Simulate delay between chunks...
+        }
+    }, 200, ['X-Accel-Buffering' => 'no']);
+});
