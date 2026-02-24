4ac6a8c5a3925e6e62c282d4743f4d9266986b2a
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-24 17:10:06 +0100

    Cleanup code and remove unnecessary System.Object[] references.

--- resources/js/components/AppHead.svelte
+++ resources/js/components/AppHead.svelte
@@ -1,8 +1,5 @@
 <script>
-    let {
-        title = '',
-        children,
-    } = $props();
+    let {title = '', children} = $props();
 
     const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
     const fullTitle = $derived(title ? `${title} - ${appName}` : appName);
