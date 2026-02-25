8c828bc0f80c43bb8a40726833e24d2a76bee43b
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-25 21:20:00 +0100

    Get random greeting.

--- resources/js/components/GreetingCard.svelte
+++ resources/js/components/GreetingCard.svelte
@@ -30,13 +30,13 @@
   ];
 
   const POSSABLE_NAMES = [
-    "Lucas",
-    "King",
-    "Sir",
-    "My lord",
+    "Lucas,",
+    "King,",
+    "Sir,",
+    "My lord,",
   ];
 
-  let greeting = $state('Hello, welcome to my Svelte app!');
+  let greeting = $state('');
 
   // We generate a greeting card text
   // This can consist of the following:
@@ -47,14 +47,47 @@
 
   function formatGreeting() {
     const randomGreeting = GREETING_OPTIONS[Math.floor(Math.random() * GREETING_OPTIONS.length)];
+    const timeBaseGreeting = getTimeGreeting();
+    const randomName = POSSABLE_NAMES[Math.floor(Math.random() * POSSABLE_NAMES.length)];
     const randomHelp = HELP_OPTIONS[Math.floor(Math.random() * HELP_OPTIONS.length)];
     const randomIntro = INTRO_OPTIONS[Math.floor(Math.random() * INTRO_OPTIONS.length)];
-    const randomName = POSSABLE_NAMES[Math.floor(Math.random() * POSSABLE_NAMES.length)];
-    greeting = `${randomGreeting}, ${randomName}! ${randomIntro} ${randomHelp}`;
+
+    const rand = Math.random();
+    // 30% - Time-based greeting with username
+    if (rand < 0.3) {
+      return `${timeBaseGreeting}, ${randomName}! ${randomIntro}${randomHelp}`;
+    }
+    // 20% - Simple greeting with username
+    else if (rand < 0.5) {
+      return `${randomGreeting}, ${randomName}! ${randomIntro}${randomHelp}`;
+    }
+    // 15% - Time-based greeting only
+    else if (rand < 0.65) {
+      return `${timeBaseGreeting}! ${randomIntro}${randomHelp}`;
+    }
+    // 35% - Just the help message (with Prime intro more likely here)
+    else {
+      const soloIntro = Math.random() < 0.6 ? `${randomIntro} ` : '';
+      return `${soloIntro}${randomHelp}`;
+    }
+  }
+
+  function getTimeGreeting() {
+    const hour = new Date().getHours();
+
+    if (hour >= 5 && hour < 12) {
+      return 'Good morning';
+    } else if (hour >= 12 && hour < 17) {
+      return 'Good afternoon';
+    } else if (hour >= 17 && hour < 22) {
+      return 'Good evening';
+    } else {
+      return 'Good night';
+    }
   }
 
   onMount(() => {
-    formatGreeting();
+    greeting = formatGreeting();
   });
 </script>
 
