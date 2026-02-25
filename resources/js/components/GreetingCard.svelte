01f0550925427e3195bb6f2477088b12a1a99b16
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-25 21:06:11 +0100

    Add formatting to greeting.

--- resources/js/components/GreetingCard.svelte
+++ resources/js/components/GreetingCard.svelte
@@ -1,5 +1,15 @@
 <script>
+  import { onMount } from 'svelte';
+
   let greeting = $state('Hello, welcome to my Svelte app!');
+
+  function formatGreeting() {
+    greeting = 'Hello, welcome to my Svelte app!!!';
+  }
+
+  onMount(() => {
+    formatGreeting();
+  });
 </script>
 
 <div class="greeting-card">
