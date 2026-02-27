3e33f41717b9c16ad418f14d95b8076ab03cdde6
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-27 18:40:07 +0100

    Copy over logo.

--- resources/js/components/StatusCard.svelte
+++ resources/js/components/StatusCard.svelte
@@ -1,10 +1,12 @@
 <script>
   import statusCard from '@/lib/status-card.js';
+  import Logo from './Logo.svelte';
 
   let greeting = $state(statusCard.getRandomGreeting());
  
 </script>
 
 <div class="status-card">
+  <Logo size={280} thinking={true} />
   <h1>{greeting}</h1>
 </div>
