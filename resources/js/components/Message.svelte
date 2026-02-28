4a5c2bdc7ead8e813cade19267424a99ca379c42
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-28 17:13:27 +0100

    Use onMount.

--- resources/js/components/Message.svelte
+++ resources/js/components/Message.svelte
@@ -1,7 +1,7 @@
 <script>
   import { gfmPlugin } from 'svelte-exmarkdown/gfm';
   import Markdown from 'svelte-exmarkdown';
-  import { untrack } from 'svelte';
+  import { untrack, onMount } from 'svelte';
   import '../../scss/components/message.scss';
 
   let { role, text, isLast = false } = $props();
@@ -10,25 +10,18 @@
   let visibleText = $state('');
 
   let words = [];
-  let interval;
 
   $effect(() => {
-    const current = text.split(/\s+/);
+    words = text.split(/\s+/);
+  });
 
-    // update backing word list without resetting progress
-    words = current;
-
-    // if no interval running, start one
-    if (!interval) {
-      interval = setInterval(() => {
-        if (visibleCount < words.length) {
-          visibleCount++;
-          untrack(() => {
-            visibleText = words.slice(0, visibleCount).join(' ');
-          });
-        }
-      }, 50);
-    }
+  onMount(() => {
+    setInterval(() => {
+      if (visibleCount < words.length) {
+        visibleCount++;
+        visibleText = words.slice(0, visibleCount).join(' ');
+      }
+    }, 50);
   });
 </script>
 
