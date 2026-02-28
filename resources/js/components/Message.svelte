ec48ed8dc0c78e4afaea0e7f7bcc37a51d028360
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-02-28 17:15:06 +0100

    Extract function for instant update.

--- resources/js/components/Message.svelte
+++ resources/js/components/Message.svelte
@@ -16,13 +16,20 @@
   });
 
   onMount(() => {
-    setInterval(() => {
-      if (visibleCount < words.length) {
-        visibleCount++;
-        visibleText = words.slice(0, visibleCount).join(' ');
-      }
-    }, 50);
+    setInterval(updateVisibleText, 50);
+    updateVisibleText();
   });
+
+  function updateVisibleText() {
+    if (visibleCount < words.length && isLast && role === 'assistant') {
+      visibleCount++;
+      visibleText = words.slice(0, visibleCount).join(' ');
+    } else {
+      visibleCount = words.length;
+      visibleText = text;
+    }
+  };
+
 </script>
 
 <div class="message" class:user={role === 'user'} class:assistant={role === 'assistant'}>
