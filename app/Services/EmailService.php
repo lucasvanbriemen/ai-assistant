b236b45e61ba9f1fdda91d87cdfded7ba898269c
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-03-01 16:46:03 +0100

    Add email tool function calls.

--- app/Services/EmailService.php
+++ app/Services/EmailService.php
@@ -13,7 +13,39 @@ class EmailService
         // User: "Show me all emails from Alice"
         // Gets email from Alice (from memory)
         // gets all email contents from api and summarizes them to show in UI
+
+        [
+            'name' => 'search_emails',
+            'description' => 'Search for emails based on filters and provide a summary.',
+            'input_schema' => [
+                'type' => 'object',
+                'properties' => [
+                    'location' => [
+                        'type' => 'string',
+                        'description' => 'The location to get the weather for. This should be a city or country name.',
+                    ],
+                ],
+                'required' => ['location'],
+            ],
+        ],
+        [
+            'name' => 'read_email',
+            'description' => 'Read the content of a specific email.',
+            'input_schema' => [
+                'type' => 'object',
+                'properties' => [
+                    'email_id' => [
+                        'type' => 'string',
+                        'description' => 'The ID of the email to read.',
+                    ],
+                ],
+                'required' => ['email_id'],
+            ],
+        ]
     ];
 
-    public const TOOL_FUNCTION_MAP = [];
+    public const TOOL_FUNCTION_MAP = [
+        'search_emails' => 'searchEmails',
+        'read_email' => 'readEmail',
+    ];
 }
